581d5daa57075c4222f3e989cede05e641594942
Harshit-3905 <harshitjoshileet@gmail.com>
2023-10-03 19:30:38 +0530

    Added Login Page

---
+++ frontend/src/Pages/LoginPage.js
@@ -0,0 +1,62 @@
+import { React, useEffect } from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+
+import Login from "../components/Authentication/Login";
+import SignUp from "../components/Authentication/SignUp";
+
+const Homepage = () => {
+  const history = useHistory();
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+    if (user) {
+      history.push("/chats");
+    }
+  }, [history]);
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+        textAlign={"center"}
+      >
+        <Text fontSize={"3xl"} fontFamily={"Satisfy"} color="black">
+          Task Tracker
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default Homepage;
