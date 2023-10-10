b1afb76d35346a4490be60fd055d0a6847f6c3fa
Harshit-3905 <harshitjoshileet@gmail.com>
2023-10-10 19:39:52 +0530

    Update User Schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,12 @@ const userSchema = mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    numberofTasksCompleted: { type: Number, default: 0 },
+    pic: {
+      type: String,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
   },
   { timestamps: true }
 );
