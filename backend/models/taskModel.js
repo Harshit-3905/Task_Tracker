64f1eb083396ab7939852c4d205bc4a62c47a4cd
Harshit-3905 <harshitjoshileet@gmail.com>
2023-10-12 19:47:44 +0530

    Updated Task Model

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
-const taskSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  authorId: { type: String, required: true },
-  completed: { type: Boolean, default: false },
-});
+const taskSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    authorId: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Task = mongoose.model("Task", taskSchema);
 module.exports = Task;
