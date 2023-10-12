45cb5d8b7c6fadf57529127571ed851ec061e3ce
Harshit-3905 <harshitjoshileet@gmail.com>
2023-10-12 19:49:43 +0530

    Updated UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
 import HomePage from "./Pages/HomePage";
+import ProfilePage from "./Pages/ProfilePage";
+import LeaderboardPage from "./Pages/LeaderboardPage";
 
 function App() {
   return (
     <div className="App">
       <Route path="/" component={LoginPage} exact />
       <Route path="/home" component={HomePage} />
+      <Route path="/profile" component={ProfilePage} />
+      <Route path="/leaderboard" component={LeaderboardPage} />
     </div>
   );
 }
