09b39e581a4e8f4fd3ff098c528da3edf3a55f37
Harshit-3905 <harshitjoshileet@gmail.com>
2023-10-12 19:48:10 +0530

    Updated UI

--- frontend/src/Pages/ProfilePage.js
+++ frontend/src/Pages/ProfilePage.js
@@ -2,10 +2,10 @@ import React from "react";
 import { VStack, HStack, Box } from "@chakra-ui/react";
 import Navbar from "../components/miscellaneous/Navbar";
 import Quote from "../components/miscellaneous/Quote";
+import Profile from "../components/miscellaneous/Profile";
 import Sidebar from "../components/miscellaneous/Sidebar";
-import Task from "../components/miscellaneous/Task";
 
-const HomePage = () => {
+const ProfilePage = () => {
   return (
     <Box width="100vw" height="100vh" padding="10px 20px 10px 20px">
       <HStack w="100%" h="100%">
@@ -14,7 +14,7 @@ const HomePage = () => {
           <Box w="100%">
             <Navbar />
           </Box>
-          <Task />
+          <Profile />
           <Quote />
         </VStack>
       </HStack>
@@ -22,4 +22,4 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+export default ProfilePage;
