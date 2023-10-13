a4954a326d4cec3a2306d84caa5dc66ad7be34ff
Harshit-3905 <harshitjoshileet@gmail.com>
2023-10-14 00:51:14 +0530

    Added Leaderboard

--- frontend/src/components/miscellaneous/Leaderboard.js
+++ frontend/src/components/miscellaneous/Leaderboard.js
@@ -1,10 +1,32 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import {
+  Box,
+  Text,
+  Table,
+  Tbody,
+  Tr,
+  Td,
+  TableContainer,
+  Center,
+  Thead,
+  Th,
+  Avatar,
+  HStack,
+} from "@chakra-ui/react";
 import { useColorModeValue } from "@chakra-ui/color-mode";
+import { useState } from "react";
+import axios from "axios";
 
 const Leaderboard = () => {
   const color = useColorModeValue("black", "white");
   const bgColor = useColorModeValue("white", "gray.700");
+  const [topFive, setTopFive] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/user/leaderboard").then((response) => {
+      const leaderboardData = response.data;
+      setTopFive(leaderboardData.slice(0, 10));
+    });
+  }, []);
   return (
     <Box
       d="flex"
@@ -15,8 +37,39 @@ const Leaderboard = () => {
       borderRadius="lg"
       borderWidth="1px"
       textAlign={"center"}
+      color={color}
     >
-      Leaderboard
+      <Text fontSize="20px">Leaderboard</Text>
+      <Center>
+        <TableContainer pt={6} p={4} width="80%">
+          <Table
+            variant="striped"
+            colorScheme="teal"
+            size={"sm"}
+            textAlign={"center"}
+          >
+            <Thead>
+              <Tr>
+                <Th>Name</Th>
+                <Th>No. of Task Completed</Th>
+              </Tr>
+            </Thead>
+            <Tbody color={color} fontSize="15px">
+              {topFive.map((user) => (
+                <Tr>
+                  <Td>
+                    <HStack>
+                      <Avatar src={user.pic} size={"sm"}></Avatar>
+                      <Text>{user.name}</Text>
+                    </HStack>
+                  </Td>
+                  <Td textAlign={"center"}>{user.numberofTasksCompleted}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Center>
     </Box>
   );
 };
