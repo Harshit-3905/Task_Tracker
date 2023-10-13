d58fa8ba12f6217ca993500b376aef70d3c898db
Harshit-3905 <harshitjoshileet@gmail.com>
2023-10-13 22:58:44 +0530

    Configured Extension

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,17 @@ import LoginPage from "./Pages/LoginPage";
 import HomePage from "./Pages/HomePage";
 import ProfilePage from "./Pages/ProfilePage";
 import LeaderboardPage from "./Pages/LeaderboardPage";
+import PomodoroPage from "./Pages/PomodoroPage";
 
 function App() {
+  // eslint-disable no undef
   return (
     <div className="App">
       <Route path="/" component={LoginPage} exact />
       <Route path="/home" component={HomePage} />
       <Route path="/profile" component={ProfilePage} />
       <Route path="/leaderboard" component={LeaderboardPage} />
+      <Route path="/pomodoro" component={PomodoroPage}></Route>
     </div>
   );
 }
