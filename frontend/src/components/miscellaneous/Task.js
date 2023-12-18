3377b4be18345a1c8b9d6621c2ee8e360265eda7
Harshit-3905 <harshitjoshileet@gmail.com>
2023-12-18 18:51:05 +0530

    Update Task component height in Task.js

--- frontend/src/components/miscellaneous/Task.js
+++ frontend/src/components/miscellaneous/Task.js
@@ -5,7 +5,7 @@ import { Box } from "@chakra-ui/react";
 
 const Task = () => {
   return (
-    <Box height="85%" w="100%" marginBottom={4} gap={-2}>
+    <Box height="80%" w="100%" marginBottom={4} gap={-2}>
       <AddTask />
       <TaskContainer />
     </Box>
